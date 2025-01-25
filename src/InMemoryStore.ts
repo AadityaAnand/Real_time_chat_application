bd296f08b28f1d255b7c7735b7051b3f8f3adb9d
Aaditya Anand <aadityaanand@Mac.hsd1.md.comcast.net>
2025-01-24 20:27:03 -0500

    Upvote

--- src/InMemoryStore.ts
+++ src/InMemoryStore.ts
@@ -1,4 +1,5 @@
 import { Chat, Store, UserId } from "./store/Store";
+let globalChatId = 0;
 export interface Room{
     roomId: string;
     chats: Chat[] 
@@ -28,13 +29,21 @@ export class InMemoryStore implements Store{
             return
         }
         room.chats.push({
+            id: (globalChatId++).toString(),
             userId,
             name,
             message,
             upvotes:[]
         })
     }
-    upvote(userId: UserId, room: string, chatId: string) {
-        
+    upvote(userId: UserId, roomId: string, chatId: string) {
+        const room = this.store.get(roomId);
+        if(!room){
+            return
+        }
+        const chat = room.chats.find(({id}) => id == chatId);
+        if (chat){
+            chat.upvotes.push(userId);
+        }
     }
 }
