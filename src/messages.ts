bb9fe2a2625d6a5826a53179606d04af9ce44984
Aaditya Anand <aadityaanand271@gmail.com>
2025-01-28 14:37:57 -0500

    Added Upvote and User Messages

--- src/messages.ts
+++ src/messages.ts
@@ -1,7 +1,25 @@
 import z from "zod";
 
-const initMessage =  z.object({
+const InitMessage =  z.object({
     name: z.string(),
     userId: z.string(),
     roomId: z.string(),
 })
+
+export type InitMessageType = z.infer<typeof InitMessage>;
+
+export const UserMessage = z.object({
+    userId: z.string(),
+    roomId: z.string(),
+    message: z.string()
+})
+
+export type UserMessageType = z.infer<typeof UserMessage>;
+
+export const UpvoteMessage = z.object({
+    userId: z.string(),
+    roomId: z.string(),
+    chatId: z.string()
+})
+
+export type UpvoteMessageType = z.infer<typeof UpvoteMessage>;
